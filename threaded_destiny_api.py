ede9f2b39a46a5aac8f91cbd81fc7499f4d4a603
Hansohl Kim <hansohl@stanford.edu>
2016-11-22 01:24:28 -0800

    Added clan/group methods

--- threaded_destiny_api.py
+++ threaded_destiny_api.py
@@ -10,8 +10,7 @@ from urllib import urlencode
 #utility function for an HTTPS GET request to Bungie API with extension urlext
 #reads response body as json
 def request(urlext):
-	url = urlparse('https://www.bungie.net/Platform/Destiny/' + urlext)
-	#url_qs = urlencode(parse_qs(url.query[:-1]), True)
+	url = urlparse('https://www.bungie.net/Platform/' + urlext)
 	conn = httplib.HTTPSConnection(url.netloc)
 	conn.request('GET', url.path + "?" + url.query, headers={'X-Api-Key': 'f6736009a38a4707b549422b1bd69ea4'})
 	res = conn.getresponse()
@@ -22,23 +21,76 @@ def request(urlext):
 	
 #single argument per list item when mapping
 def getMembershipId(username):
-	urlext = 'SearchDestinyPlayer/2/' + username + '/'
+	urlext = 'Destiny/SearchDestinyPlayer/2/' + username + '/'
 	result = request(urlext)
 	return result["Response"][0]["membershipId"]
 
+#wrapper for getMembershipId that also returns the username label	
+def getLabeledMembershipId(username):
+	return (username, getMembershipId(username))
+
+#single argument per list item when mapping
+def getAccount(membershipId):
+	urlext = 'Destiny/2/Account/' + membershipId + '/Summary/'
+	result = request(urlext)
+	return result
+
 #single argument per list item when mapping
 def getCharacterIds(membershipId):
-	urlext = '2/Account/' + membershipId + '/'
-	result = request(urlext)
-	characters = result["Response"]["data"]["characters"]
+	account = getAccount(membershipId)
+	characters = account["Response"]["data"]["characters"]
 	charIdList = [character["characterBase"]["characterId"] for character in characters]
 	return charIdList
+
+#single argument per list item when mapping	
+def getClans(membershipId):
+	urlext = 'User/GetBungieAccount/' + membershipId + '/2/'
+	result = request(urlext)
+	#if len(result["Response"]["relatedGroups"]) >  1:
+	#	print "MULTIPLE RELATED GROUPS"
+	#return [group for group in result["Response"]["relatedGroups"]]
+	return [clan["groupId"] for clan in result["Response"]["clans"]]
+	
+#wrapper for getMemberClans that also returns the memId label
+def getLabeledClans(membershipId):
+	clans = getClans(membershipId)
+	return (membershipId, clans)
+
+def getClanId(clanName):
+	urlext = 'Group/Name/'  + clanName + '/'
+	result = request(urlext)
+	return result["Response"]["detail"]["groupId"]
+
+def getClanName(groupId):
+	urlext = 'Group/' + groupId + '/'
+	result = request(urlext)
+	return result["Response"]["detail"]["name"]
+	#result["Response"]["detail"]["clanCallsign"]
+
+#wrapper for getClanName that also returns the groupId label
+def getLabeledClanName(groupId):
+	name = getClanName(groupId)
+	return (groupId, name)
+	
+#get all memberIds for a clan with groupId, initial call does not need page
+def getClanMembers(groupId, page=1):
+	urlext = 'Group/' + groupId + '/ClanMembers/?platformType=2&currentPage=' + str(page)
+	result = request(urlext)
+	memberData = result["Response"]["results"]
+	members = [member["destinyUserInfo"]["membershipId"] for member in memberData]
+	if result["Response"]["hasMore"]:
+		members.extend(getClanMembers(groupId, page+1))
+	return members
+
+def getLabeledClanMembers(groupId):
+	members = getClanMembers(groupId)
+	return (groupId, members)
 
 #tuple of three arguments per list item when mapping
 #args is (membershipId, characterId, mode)
 def getCharacterActivity(args):
 	membershipId, characterId, mode = args
-	urlext = 'Stats/ActivityHistory/2/' + membershipId + '/' + characterId + '/?mode=' + mode
+	urlext = 'Destiny/Stats/ActivityHistory/2/' + membershipId + '/' + characterId + '/?mode=' + mode
 	result = request(urlext)
 	if "activities" not in result["Response"]["data"]:
 	    print "Could not find activities"
@@ -51,7 +103,7 @@ def getCharacterActivity(args):
 
 #single argument per list item when mapping
 def getActivityUsernames(activityId):
-	urlext = 'Stats/PostGameCarnageReport/' + activityId + '/'
+	urlext = 'Destiny/Stats/PostGameCarnageReport/' + activityId + '/'
 	result = request(urlext)
 	players = result["Response"]["data"]["entries"]
 	usernames = [entry["player"]["destinyUserInfo"]["displayName"] for entry in players]
@@ -62,6 +114,8 @@ def getActivityUsernames(activityId):
 def worker(q, outq, targetFunc):
 	while True:
 		args = q.get()
+		if not args: #args is None (sentinel)
+			return #shutdown thread
 		result = targetFunc(args)
 		outq.put(result)
 		print "recorded response"
@@ -110,3 +164,4 @@ def getActivityMembers(username, mode='None', limit=25, nthreads=25):
 		if membername != username:
 			result.append((str(membername), count))
 	return sorted(result, key=lambda x: x[1], reverse=True)
+
